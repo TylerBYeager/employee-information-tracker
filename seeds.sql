15c559fe257034379e8948ee39f72898962c88de
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-22 18:48:40 -0700

    add department_t seeds

---
+++ seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department_t (name)
+VALUE 
+("Sales");
+
+INSERT INTO department_t (name) 
+VALUE
+("Engineering");
+
+INSERT INTO department_t (name)
+VALUE 
+("Finance");
+
+INSERT INTO department_t (name)
+VALUE 
+("Legal");
+<-- department table information --> 
+
+
+INSERT INTO 
