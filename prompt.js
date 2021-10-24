57787e529dfc21f91615fdcf10d1ab6a16b28e1f
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-23 22:15:33 -0700

    add prompt initial questions and response

---
+++ prompt.js
@@ -0,0 +1,36 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+
+
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     port: 3001,
+//     user: "root",
+//     password: "password",
+//     database: "company_db"
+// });
+
+const questions = async () => {
+    return inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Please select your desired action",
+                name: "choice",
+                choices: [
+                    "View all Departments?",
+                    "View all employee roles?",
+                    "View all employees?",
+                    "Add new company department?",
+                    "Add new role within the company?",
+                    "Add new employee?",
+                    "Update and existing employee's role?"
+                ]
+            },
+        ])
+        .then(response => {
+            console.log(response);
+        });
+}
+questions();
